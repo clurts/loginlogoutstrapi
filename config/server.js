0ff4516592a414422b1b1835257cded6263470c8
clurts <clu@rts.dk>
2021-06-25 09:40:45 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e625fb8f13bcf613be066a346c9e0708'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b46321ad-2e2b-4069-a234-5365746375c4'
+};
